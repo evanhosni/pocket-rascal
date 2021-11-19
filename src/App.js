ae968e7cfe23a99cc5f27ebca8388b9a9214791e
evanhosni <evankhaledhosni@gmail.com>
2021-11-19 11:07:46 -0800

    initial commit. created react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './style.css';
+// import './libraries/matter'
+import './rascal'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {/* <input type="checkbox" id="devMode" onclick="devMode()">
+    <label for="devMode">devMode</label><br> */}
+    <button id="checkCoor" onclick="checkCoor()">checkCoordinates</button>
+    <button id="changeSelections" onclick="changeSelections()">changeSelections</button>
+    <div id="canvas_container"></div>
+  </div>
   );
 }
 
