539d5c075ad8c9b63cf865516934a3f4b0d69ea5
lindsfitz <lindsaytfitz@gmail.com>
2021-11-23 13:06:58 -0800

    lots of pages/component updates

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import Navigation from './Navigation';
-import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Scene from './Scene';
 import Minigame from './pages/Minigame';
+import CreateRascal from './pages/CreateRascal/index'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 export default function ContentContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('Login');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'SignUp') {
+      return <SignUp />;
     }
     if (currentPage === 'Login') {
       return <Login />;
@@ -19,6 +21,13 @@ export default function ContentContainer() {
     if (currentPage === 'Scene') {
       return <Scene />;
     }
+    if (currentPage === 'CreateRascal'){
+      return (
+      <div>
+        <CreateRascal />
+        <Scene />
+      </div>
+      )}
     return <Minigame />;
   };
 
---
+++ src/components/pages/CreateRascal/StepCreate.js
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'white',
+  },
+});
+
+
+
+
+
+
+export default function VerticalLinearStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const [color, setColor] = React.useState('');
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const classes = useStyles();
+
+  const steps = [
+    {
+      label: 'Choose a name for your Rascal:',
+      description:
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField className={classes.root} id="standard-basic" label="Enter name here" variant="standard" />
+        </Box>
+    },
+    {
+      label: 'Select a body for your pocket Rascal:',
+      description:
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Choose your Rascal's body:</FormLabel>
+          <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
+            <FormControlLabel value="female" control={<Radio />} label="Body1" />
+            <FormControlLabel value="male" control={<Radio />} label="Body2" />
+            <FormControlLabel value="other" control={<Radio />} label="Body3" />
+            <FormControlLabel
+              value="disabled"
+              disabled
+              control={<Radio />}
+              label="other"
+            />
+          </RadioGroup>
+        </FormControl>
+    },
+    {
+      label: 'Choose a color for your new friend:',
+      description:
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel className={classes.root} id="demo-simple-select-label">Color</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={color}
+              label="Color"
+              onChange={handleColorChange}
+            >
+              <MenuItem value={10}>Blue</MenuItem>
+              <MenuItem value={20}>Green</MenuItem>
+              <MenuItem value={30}>Red</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+    },
+  ];
+
+
+  return (
+    <Box sx={{ maxWidth: 400 }} className={classes.root}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel 
+              optional={
+                index === 2 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
+}
