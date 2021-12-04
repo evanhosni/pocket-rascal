947698e8f2784f74cd536e029ef289827cd95abd
evanhosni <evankhaledhosni@gmail.com>
2021-12-03 18:00:06 -0800

    updated store. looks pretty good

--- src/components/pages/Dashboard/Store/Nose.js
+++ src/components/pages/Dashboard/Store/Nose.js
@@ -6,6 +6,8 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import IconButton from "@mui/material/IconButton";
 import Snackbar from '@mui/material/Snackbar';
 import CloseIcon from '@mui/icons-material/Close';
+import Button from "@mui/material/Button";
+import "./store.css";
 
 const noseData = [
     {
@@ -26,60 +28,88 @@ const noseData = [
 
 export default function StoreNose(props) {
 
-    //functions for snackbar for successful purchase from store 
-    const [open, setOpen] = React.useState(false);
+  //functions for snackbar for successful purchase from store
+  const [open, setOpen] = React.useState(false);
 
-    const handleClick = () => {
-        setOpen(true);
-    };
+  const handleClick = () => {
+    setOpen(true);
+  };
 
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpen(false);
-    };
-
-    const action = (
-        <React.Fragment>
-            <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={handleClose}
-            >
-                <CloseIcon fontSize="small" />
-            </IconButton>
-        </React.Fragment>
-    );
-
-    //functions for snackbar for failed purchase from store
-    const [openFail, setOpenFail] = React.useState(false);
-
-    const handleFail = () => {
-        setOpenFail(true);
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
 
-    const handleCloseFail = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+    setOpen(false);
+  };
 
-        setOpenFail(false);
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  //functions for snackbar for failed purchase from store
+  const [openFail, setOpenFail] = React.useState(false);
+
+  const handleFail = () => {
+    setOpenFail(true);
+  };
+
+  const handleCloseFail = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenFail(false);
+  };
+
+  const actionFail = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleCloseFail}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+    //functions for get more coins dialog
+    const [moreCoins, setMoreCoins] = React.useState(false);
+
+    const handleMoreCoins = () => {
+      setMoreCoins(true);
     };
-
-    const actionFail = (
-        <React.Fragment>
-            <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={handleCloseFail}
-            >
-                <CloseIcon fontSize="small" />
-            </IconButton>
-        </React.Fragment>
+  
+    const handleCloseCoins = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+  
+      setMoreCoins(false);
+    };
+  
+    const moreCoinsFail = (
+      <React.Fragment>
+        <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={handleCloseCoins}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      </React.Fragment>
     );
 
     const saveNewItem = (item) => {
@@ -103,55 +133,88 @@ export default function StoreNose(props) {
     }
 
 
+    const checkIfOwned = (item) => {
+        var tempArray = []
+        for (let i = 0; i < props.unlockedItems.length; i++) {
+            tempArray.push(props.unlockedItems[i].name);
+        }
+        console.log(tempArray)
+        console.log(item.img)
+          if (tempArray.includes(item.img)) {
+            return (
+                <div className="alreadyowned">
+                    [<span>ALREADY OWNED</span>]
+                </div>
+            )
+        } else {
+            return (
+              <div className="priceandpurchase">
+                  <div className="price">{item.price}<span>¢</span></div>
+                  <Button id="purchase" onClick={() => {purchaseItem(item)}}>BUY</Button>
+              </div>
+            )
+          }
+      }
 
-    return (
-        <div>
 
-            <ImageList sx={{ width: '95%', height: 400 }} cols={2} rowHeight={164} style={{ overflow: 'scroll', padding: 20 }}>
-                {noseData.map((item) => (
-                    <ImageListItem key={item.img}>
-                        <img
-                            src={`./assets/${item.img}.png`}
-                            srcSet={`./assets/${item.img}.png`}
-                            alt={item.title}
-                            style={{ objectFit: 'cover', height: '90px', objectPosition: '45% -2px' }}
-                            loading="lazy"
-                        />
-                        <ImageListItemBar
-                            title={item.title}
-                            subtitle={`${item.price}¢`}
-                            actionIcon={
-                                <IconButton
-                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                    aria-label={`info about ${item.title}`}
-                                    onClick={() => {
-                                        purchaseItem(item)
-                                    }
-                                    }
-                                >
-                                    <AddCircleIcon />
-                                </IconButton>
-                            }
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-            <Snackbar
-                open={open}
-                autoHideDuration={6000}
-                onClose={handleClose}
-                message="Item Saved"
-                action={action}
-            />
-            <Snackbar
-                open={openFail}
-                autoHideDuration={6000}
-                onClose={handleCloseFail}
-                message="You need more coins for this item!"
-                action={actionFail}
-            />
+      return (
+        <div style={{display: 'flex', flexDirection:'column'}}>
+          <div style={{ padding: 20}}>
+            {noseData.map((item) => (
+              <div
+                obj={item}
+                key={item}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  paddingBottom: '3px',
+                  paddingTop: '3px',
+                  borderBottom: "dashed black 3px"
+                }}
+              >
+                <div className="imgandtitle">
+                    <div className="itemforsale">
+                    <img
+                        src={`./assets/${item.img}.png`}
+                        alt={item.title}
+                        style={{
+                        objectFit: "cover",
+                        height: "180px",
+                        objectPosition: "-45px -60px",
+                        // backgroundColor:'white'
+                        }}
+                        loading="lazy"
+                    />
+                    </div>
+                    <div className="title">{item.title}</div>
+                </div>
+                    {checkIfOwned(item)}
+              </div>
+            ))}
+          </div>
+          <Button id="clickbait" onClick={() => {handleMoreCoins()}}><div>Get more <span id="cents">¢</span> !</div></Button>
+          <Snackbar
+            open={open}
+            autoHideDuration={5000}
+            onClose={handleClose}
+            message="Item Saved"
+            action={action}
+          />
+          <Snackbar
+            open={openFail}
+            autoHideDuration={5000}
+            onClose={handleCloseFail}
+            message="You need more coins for this item!"
+            action={actionFail}
+          />
+          <Snackbar
+            open={moreCoins}
+            autoHideDuration={5000}
+            onClose={handleCloseCoins}
+            message="Play minigames to earn more coins!"
+            action={moreCoinsFail}
+          />
         </div>
-
-
-    )
+      );
 }
